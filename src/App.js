d4598936d4213f0cf9f1e43138b21057a98dc210
nadjaivk984 <nadjaivkovic984@gmail.com>
2021-09-09 20:01:20 +0200

    Created Dummy Candidate report page & add Router

--- src/App.js
+++ src/App.js
@@ -2,14 +2,23 @@ import { Fragment } from "react";
 import Footer from "./app/components/footer/Footer";
 import Header from "./app/components/header/Header";
 import Home from "./app/pages/home/Home";
+import CandidateReport from "./app/pages/CandidateReport/CandidateReport.jsx";
+import{BrowserRouter, Route,Switch} from "react-router-dom";
 
 const App = () => {
   return (
+    <BrowserRouter>
     <Fragment>
       <Header />
-      <Home />
+      <Switch>
+        <Route exact path={"/"} component={Home} />
+        <Route path={"/candidateReport"} component={CandidateReport} />
+      
+        </Switch>
+     
       <Footer />
     </Fragment>
+    </BrowserRouter>
   );
 };
 
