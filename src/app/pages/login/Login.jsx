b935615e7ebebaa9d60b296b3e3c24791412fb16
Daniel-Avramovic <danijelgti@gmail.com>
2021-09-09 19:15:53 +0200

    moving components

---
+++ src/app/pages/login/Login.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Login = () => {
+    return(
+        <main>
+            <form>
+                <input type="text" placeholder="email" />
+                <input type="password" placeholder="password" />
+                <button>Back to</button>
+                <button>Submit</button>
+            </form>
+        </main>
+    )
+}
