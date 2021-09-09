d3fa7d4b22738470f8f5fd9bdcb670beac77a99b
Daniel-Avramovic <danijelgti@gmail.com>
2021-09-09 19:14:46 +0200

    moving componetns

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { Fragment } from "react";
 import Footer from "./app/components/footer/Footer";
 import Header from "./app/components/header/Header";
-import Home from "./app/components/home/Home";
+import Home from "./app/pages/home/Home";
 
 const App = () => {
   return (
