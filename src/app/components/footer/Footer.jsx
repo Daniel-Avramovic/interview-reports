fd9a99a3b23a9f859355188f62ee35c4db319c9f
Daniel-Avramovic <danijelgti@gmail.com>
2021-09-09 18:49:09 +0200

    create header, home, and footer componnent

---
+++ src/app/components/footer/Footer.jsx
@@ -0,0 +1,12 @@
+
+import React from 'react';
+
+const Footer = () => {
+    return(
+        <footer>
+            Bit project
+        </footer>
+    )
+};
+
+export default Footer
---
+++ src/app/components/header/Header.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header>
+      <div className="navbar-fixed">
+        <nav>
+          <div className="nav-wrapper">
+            <a href="#!" classname="brand-logo">
+              JobBook
+            </a>
+            <ul className="right hide-on-med-and-down">
+              <li>
+                <a href="sass.html">Login</a>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
