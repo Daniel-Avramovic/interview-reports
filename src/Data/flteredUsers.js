bf0cfbd0d011507ceacfeeef8ca027bea1c2e3e1
nadjaivk984 <nadjaivkovic984@gmail.com>
2021-09-11 17:30:04 +0200

    implement Searchbar

--- src/Data/flteredUsers.js
+++ src/Data/flteredUsers.js
@@ -3,7 +3,7 @@ export const filterUser = (users, val) => {
    return users.filter(user => {
     if(val === ""){
       return user
-    }else if(user.name.first.toLowerCase().includes(val.toLowerCase())){
+    }else if(user.name.toLowerCase().includes(val.toLowerCase())){
       return user
     }else{
       return false;
