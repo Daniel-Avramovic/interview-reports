9442bdadf6ca955cb097893ea16fabdda3862c0f
nadjaivk984 <nadjaivkovic984@gmail.com>
2021-09-11 10:45:13 +0200

    Dummy home candidate page

--- src/app/pages/home/Home.jsx
+++ src/app/pages/home/Home.jsx
@@ -1,34 +1,38 @@
-import React from 'react';
-import "./home.css"
+import React from "react";
+import "./home.css";
 
 const Home = () => {
-
-
-    return(
-      
-           <main>
-             <h1>Welcome</h1>
-          {/* <div className="row card2 cardsize">
+  const img =
+    "https://clinicforspecialchildren.org/wp-content/uploads/2016/08/avatar-placeholder.gif";
+  const dummyCandidates = [
+    { name: "first", email: "first@gmail.com" },
+    { name: "second", email: "second@gmail.com" },
+    { name: "third", email: "third@gmail.com" },
+    { name: "third", email: "third@gmail.com" },
+    { name: "third", email: "third@gmail.com" },
+    { name: "third", email: "third@gmail.com" },
+  ];
+  return (
+    <main className="container">
+      {dummyCandidates.map((candidate) => {
+        return (
+          <div className="row card2 cardsize">
             <div className="col s10 m10">
               <div className="card">
-               <div className="card-image imageavatar ">
-             <img src="https://clinicforspecialchildren.org/wp-content/uploads/2016/08/avatar-placeholder.gif" ></img>
+                <div className="card-image imageavatar ">
+                  <img src={img} alt="No img"></img>
+                </div>
+
+                <span className="card-title name">{candidate.name}</span>
+                <span className="card-email email">{candidate.email}</span>
+                <div className="card-content"></div>
               </div>
-             
-              <span className="card-title name">Name</span>
-              <span className="card-email email">ime@gmail.com</span>
-          <div className="card-content">
-    
-
-        
-         </div>
-    
-        </div>
-   </div>
-   </div> */}
-
-        </main>
-    )
+            </div>
+          </div>
+        );
+      })}
+    </main>
+  );
 };
 
 export default Home;
