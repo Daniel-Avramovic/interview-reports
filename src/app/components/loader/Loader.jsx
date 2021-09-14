b3a1706330a096f12cd67ae4a5163e058c0e5676
ansew <vesna983@gmail.com>
2021-09-14 19:10:04 +0200

    login touch css

---
+++ src/app/components/loader/Loader.jsx
@@ -0,0 +1,13 @@
+import './loader.css';
+
+const Loader = () => {
+    return (
+        <main>
+            <div className="load-holder">
+                <div className="clock-loader"></div>
+            </div>
+        </main>
+    )
+}
+
+export default Loader;
--- src/app/pages/login/Login.jsx
+++ src/app/pages/login/Login.jsx
@@ -24,8 +24,8 @@ const Login = ({ submit, email, password, onChangeEmail, onChangePassword }) => 
         />
         </div>
         <div className="buttons">
-        <button>Back to Home</button>
-        <button onClick={submit}>Log in</button>
+        <button className="single-button">Back to Home</button>
+        <button className="single-button" onClick={submit}>Log in</button>
         </div>
         </form>
     </main>
