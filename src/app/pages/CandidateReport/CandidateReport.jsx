068993ec77bc84ddf4dca630d7dd8aa91a19f9d0
Daniel-Avramovic <danijelgti@gmail.com>
2021-09-14 02:05:54 +0200

    add component for single candidatUI

--- src/app/pages/CandidateReport/CandidateReport.jsx
+++ src/app/pages/CandidateReport/CandidateReport.jsx
@@ -1,50 +1,46 @@
-import React, { useEffect, useState } from "react";
-import { formatDate } from "../../../Data/formatDate";
+import React, { Fragment, useEffect, useState } from "react";
 import { getCandidates } from "../../../services/getCandidates";
+import { getReports } from "../../../services/getReports";
 import "./candidateReport.css";
+import SingleCandidate from "./candidateReportsUI/SingleCandidate";
 
 const CandidateReport = ({ match }) => {
   const id = parseInt(match.params.id);
   const token = sessionStorage.getItem("token");
-  const img = "https://clinicforspecialchildren.org/wp-content/uploads/2016/08/avatar-placeholder.gif";
-  // const img =
-  //   "https://clinicforspecialchildren.org/wp-content/uploads/2016/08/avatar-placeholder.gif";
-  const [candidat, setCandidat] = useState({});
+  const [candidate, setCandidate] = useState({});
+  const [reports, setReports] = useState([]);
+  const [loading, setLoading] = useState(true);
   const onGetCandidates = () => {
     const get = async () => {
       const candidates = await getCandidates(token);
-      candidates.forEach((candidat) => {
-        if (candidat.id === id) {
-          setCandidat(candidat);
+      candidates.forEach((candidate) => {
+        if (candidate.id === id) {
+          setCandidate(candidate);
         }
       });
     };
     get();
   };
+  const onGetReports = () => {
+    const reports = async () => {
+      let tempRepots = [];
+      const allReports = await getReports(token);
+      allReports.forEach((report) => {
+        if (report.candidateId === id) {
+          tempRepots.push(report);
+        }
+      });
+      setReports(tempRepots);
+      setLoading(false);
+    };
+    reports();
+  };
   useEffect(onGetCandidates, [token, id]);
+  useEffect(onGetReports, [token, id]);
   return (
-    <main className="container d-flex">
-      <div className="d-flex">
-      <div>
-        <img src={img} alt="some pic" /> 
-      </div>
-      <div>
-        <p>Name:</p>
-        <p>{candidat.name}</p>
-        <p>Email:</p>
-        <p>{candidat.email}</p>
-      </div>
-      <div>
-        <p>Date of birth:</p>        
-        <p>{formatDate(candidat.birthday)}</p>
-        {console.log(new Date(candidat.birthday))}
-        <p>Education:</p>
-        <p>{candidat.education}</p>
-      </div>
-      </div>
-
-      <div>Tabela</div>
-    </main>
+    <Fragment>
+      {loading ? <h1>Loading...</h1> : <SingleCandidate candidate={candidate} reports={reports} />}
+    </Fragment>
   );
 };
 
