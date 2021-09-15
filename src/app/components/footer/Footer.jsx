ae9b40ee13e16e79e9e519cddd6f782947ff50c8
Daniel-Avramovic <danijelgti@gmail.com>
2021-09-15 15:33:12 +0200

    add css in footer

--- src/app/components/footer/Footer.jsx
+++ src/app/components/footer/Footer.jsx
@@ -1,10 +1,13 @@
-
 import React from 'react';
+import { Container } from 'react-bootstrap';
+import './footer.css';
 
 const Footer = () => {
     return(
         <footer>
-            Bit project
+            <Container>
+            <p className="styleFooter">© 2021 Copyright BIT Students</p>
+            </Container>
         </footer>
     )
 };
