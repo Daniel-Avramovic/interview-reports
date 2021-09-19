5a1fd4828209f4b369297c34ed57771cf62eb1b0
Daniel-Avramovic <danijelgti@gmail.com>
2021-09-19 21:41:53 +0200

    add steps and logic for step 1

--- src/app/pages/reports/reportsUI/ReportsUI.jsx
+++ src/app/pages/reports/reportsUI/ReportsUI.jsx
@@ -4,23 +4,21 @@ import { formatDate } from "../../../../Data/formatDate";
 import Modalset from "../../../components/modal/Modal";
 import SearchBar from "../../../components/searchBar/SearchBar";
 import "./reportsUI.css";
-import { faEye,  faTrashAlt } from "@fortawesome/free-solid-svg-icons";
+import { faEye, faTrashAlt } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { deleteReport } from "../../../../services/deleteReport";
 import { filterReport } from "../../../../Data/filterReports";
 const ReportsUI = ({ reports, value, search }) => {
   const filteredReports = filterReport(reports, value);
-  console.log(filteredReports)
-  const token = sessionStorage.getItem('token');
-  const [modal,setModal] = useState(null);
+  const token = sessionStorage.getItem("token");
+  const [modal, setModal] = useState(null);
   const close = () => {
-      setModal(null);
+    setModal(null);
   };
   return (
     <main>
-        
       <Container>
-          {modal && <Modalset closed={close} modalObj={modal} />}
+        {modal && <Modalset closed={close} modalObj={modal} />}
         <SearchBar value={value} search={search} />
         <Table className="mt-5">
           <thead>
@@ -37,8 +35,24 @@ const ReportsUI = ({ reports, value, search }) => {
                   <th>{report.candidateName}</th>
                   <th>{formatDate(report.interviewDate)}</th>
                   <th>
-                    {report.status} <div className="fRight">
-                    <button className="styleReportButton" onClick={()=>{setModal(report);}} ><FontAwesomeIcon icon={faEye} /></button><button className="styleReportButton" onClick={()=>{deleteReport(token, report.id)}}><FontAwesomeIcon icon={faTrashAlt} /></button>
+                    {report.status}
+                    <div className="fRight">
+                      <button
+                        className="styleReportButton"
+                        onClick={() => {
+                          setModal(report);
+                        }}
+                      >
+                        <FontAwesomeIcon icon={faEye} />
+                      </button>
+                      <button
+                        className="styleReportButton"
+                        onClick={() => {
+                          deleteReport(token, report.id);
+                        }}
+                      >
+                        <FontAwesomeIcon icon={faTrashAlt} />
+                      </button>
                     </div>
                   </th>
                 </tr>
